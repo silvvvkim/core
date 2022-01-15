36a208ed41362a38a25178a0e818e307a6aefc5b
silvvvkim <rmsid1312@hanyang.ac.kr>
2022-01-16 00:40:08 +0900

    Add 1) RateDiscountPolicy
    2) In MemberService and OrderService, these violate DIP(Dependency Inversion Principle).
    
    The reason is when the object that they depend changed, Service implementation class needs to change depending object class.
    
    3) So Fix to DIP, create new class that name is AppConfig.
    AppConfig role is that it decides what object they use.

--- src/main/java/hello/core/order/OrderServiceImpl.java
+++ src/main/java/hello/core/order/OrderServiceImpl.java
@@ -2,14 +2,20 @@ package hello.core.order;
 
 import hello.core.discount.DisCountPolicy;
 import hello.core.discount.FixDiscountPolicy;
+import hello.core.discount.RateDiscountPolicy;
 import hello.core.member.Member;
 import hello.core.member.MemberRepository;
 import hello.core.member.MemoryMemberRepository;
 
 public class OrderServiceImpl implements OrderService{
 
-    private final MemberRepository memberRepository = new MemoryMemberRepository();
-    private final DisCountPolicy disCountPolicy = new FixDiscountPolicy();
+    private final MemberRepository memberRepository;
+    private final DisCountPolicy disCountPolicy;
+
+    public OrderServiceImpl(MemberRepository memberRepository, DisCountPolicy disCountPolicy) {
+        this.memberRepository = memberRepository;
+        this.disCountPolicy = disCountPolicy;
+    }
 
     @Override
     public Order createOrder(Long memberId, String itemName, int itemPrice) {
