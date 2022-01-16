5d41f99877e7377b38d6887c618d422d27158ae2
silvvvkim <rmsid1312@hanyang.ac.kr>
2022-01-16 10:43:08 +0900

    Change 1) Add spring container and spring bean

--- src/main/java/hello/core/AppConfig.java
+++ src/main/java/hello/core/AppConfig.java
@@ -8,22 +8,29 @@ import hello.core.member.MemberServiceImpl;
 import hello.core.member.MemoryMemberRepository;
 import hello.core.order.OrderService;
 import hello.core.order.OrderServiceImpl;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 
+@Configuration
 public class AppConfig {
 
+    @Bean
     public MemberService memberService(){
         return new MemberServiceImpl(memberRepository());
     }
 
+    @Bean
     public OrderService orderService(){
         return new OrderServiceImpl(memberRepository(), disCountPolicy());
     }
 
-    private MemoryMemberRepository memberRepository() {
+    @Bean
+    public MemoryMemberRepository memberRepository() {
         return new MemoryMemberRepository();
     }
 
-    private DisCountPolicy disCountPolicy() {
-        return new FixDiscountPolicy();
+    @Bean
+    public DisCountPolicy disCountPolicy() {
+        return new RateDiscountPolicy();
     }
 }
