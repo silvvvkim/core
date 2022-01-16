6edc84d403ad54555299fe9181aaf6de2955b4aa
silvvvkim <rmsid1312@hanyang.ac.kr>
2022-01-16 13:25:15 +0900

    Add 1) Add ApplicationContext Bean Testcase 2) Add XmlConfiguration

--- src/test/java/hello/core/xml/XmlAppContext.java
+++ src/test/java/hello/core/xml/XmlAppContext.java
@@ -4,18 +4,18 @@ import hello.core.AppConfig;
 import hello.core.member.MemberService;
 import hello.core.member.MemberServiceImpl;
 import org.assertj.core.api.Assertions;
-import org.junit.jupiter.api.Test;
-import org.springframework.context.ApplicationContext;
-import org.springframework.context.support.GenericXmlApplicationContext;
-
-import static org.assertj.core.api.Assertions.*;
-
-public class XmlAppContext {
-
-    @Test
-    void xmlAppContext(){
-        ApplicationContext ac = new GenericXmlApplicationContext("appConfig.xml");
-        MemberService memberService = ac.getBean("memberService", MemberService.class);
-        assertThat(memberService).isInstanceOf(MemberService.class);
-    }
-}
+//import org.springframework.context.ApplicationContext;
+//import org.junit.jupiter.api.Test;
+//import org.springframework.context.support.GenericXmlApplicationContext;
+//
+//import static org.assertj.core.api.Assertions.*;
+//
+//public class XmlAppContext {
+//
+//    @Test
+//    void xmlAppContext(){
+//        ApplicationContext ac = new GenericXmlApplicationContext("appConfig.xml");
+//        MemberService memberService = ac.getBean("memberService", MemberService.class);
+//        assertThat(memberService).isInstanceOf(MemberService.class);
+//    }
+//}
